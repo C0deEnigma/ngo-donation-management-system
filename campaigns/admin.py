65da9a7d1c3efc2c8f42450cca55f949b69d99cb
Shivam Kaushik <kaushik9261@gmail.com>
2026-01-13 13:25:39 +0000

    Add custom user model and finalize read-only admin setup

--- campaigns/admin.py
+++ campaigns/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import Campaign
 
-# Register your models here.
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "goal_amount",
+        "start_date",
+        "end_date",
+        "is_active",
+    )
+
+    list_filter = (
+        "is_active",
+        "start_date",
+        "end_date",
+    )
+
+    search_fields = (
+        "title",
+    )
+
+    ordering = (
+        "-created_at",
+    )
+
+    readonly_fields = (
+        "created_at",
+    )
+
+    fieldsets = (
+        (None, {
+            "fields": ("title", "description")
+        }),
+        ("Financial Info", {
+            "fields": ("goal_amount",)
+        }),
+        ("Schedule", {
+            "fields": ("start_date", "end_date")
+        }),
+        ("Status", {
+            "fields": ("is_active",)
+        }),
+        ("Timestamps", {
+            "fields": ("created_at",)
+        }),
+    )
--- donations/admin.py
+++ donations/admin.py
@@ -1,3 +1,77 @@
 from django.contrib import admin
+from .models import Donation
 
-# Register your models here.
+
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    list_display = (
+        "registration_name",
+        "registration_user",
+        "registration_campaign",
+        "amount",
+        "payment_status",
+        "payment_provider",
+        "transaction_id",
+        "created_at",
+    )
+
+    list_filter = (
+        "payment_provider",
+        "payment_status",
+        "created_at",
+    )
+
+    search_fields = (
+        "registration__name",
+        "registration__user__username",
+        "registration__user__email",
+        "transaction_id",
+    )
+
+    raw_id_fields = (
+        "registration",
+    )
+
+    ordering = (
+        "-created_at",
+    )
+
+    readonly_fields = (
+        "registration",
+        "amount",
+        "payment_status",
+        "payment_provider",
+        "transaction_id",
+        "created_at",
+    )
+
+    fieldsets = (
+        ("Payment Info", {
+            "fields": (
+                "amount",
+                "payment_status",
+                "payment_provider",
+                "transaction_id",
+            )
+        }),
+        ("Registration Info", {
+            "fields": ("registration",)
+        }),
+        ("Timestamps", {
+            "fields": ("created_at",)
+        }),
+    )
+
+    # ---------- display helpers ----------
+
+    def registration_name(self, obj):
+        return obj.registration.name
+    registration_name.short_description = "Name"
+
+    def registration_user(self, obj):
+        return obj.registration.user
+    registration_user.short_description = "User"
+
+    def registration_campaign(self, obj):
+        return obj.registration.campaign
+    registration_campaign.short_description = "Campaign"
